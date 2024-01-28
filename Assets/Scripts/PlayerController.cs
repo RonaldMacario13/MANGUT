6aae4ccd2f912c16a15c7062b75d72a07d7ebd21
Ronald Macario <ronaldmacario13@gmail.com>
2024-01-28 20:37:50 -0300

    Adicionando a habilidade de corrida do player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,46 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     private Rigidbody2D _playerRigidbody2D;
     [SerializeField]
     private float _playerSpeed;
+    private float _playerInitialSpeed;
+    [SerializeField]
+    private float _playerRunSpeed;
     private Vector2 _playerDirection;
 
-    void Start() {
+    void Start() 
+    {
         _playerRigidbody2D = GetComponent<Rigidbody2D>();
+
+        _playerInitialSpeed = _playerSpeed;
     }
 
-    void Update() {
+    void Update() 
+    {
         _playerDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        PlayerRun();
     }
 
+    void FixedUpdate() 
+    {
         _playerRigidbody2D.MovePosition(_playerRigidbody2D.position + _playerDirection.normalized * _playerSpeed * Time.fixedDeltaTime);
+    }
+
+    void PlayerRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _playerSpeed = _playerRunSpeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            _playerSpeed = _playerInitialSpeed;
+        }
     }
 
 }
