6315d0fbf97f43551bb83873118265f176d813b9
Lucas Monteiro <lucasmonteiromelo@hotmail.com>
2024-04-30 21:49:49 -0300

    chore: add dev na tela-carregamento

---
+++ Assets/Scripts/LoadingController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Image image;
+    public Text loadingText;
+}
