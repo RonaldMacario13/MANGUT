f67677923ec1dd5331c179df9e2845c12b89eea9
Lucas Monteiro <lucasmonteiromelo@hotmail.com>
2024-05-10 21:46:11 -0300

    feat: adicionando botões e background do menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -8,8 +8,20 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private string levelName;
     //private GameObject menuPanel;
 
-    public void Play() {
+    public void PlayNewGame() {
         SceneManager.LoadScene(levelName);
+    }
+
+    public void Continue() {
+        print("TODO CONTINUE GAME");
+    }
+
+    public void Credits() {
+        print("TODO GAME CREDITS");
+    }
+
+    public void MonsterSheets() {
+        print("TODO MONSTERSHEET");
     }
 
     public void Exit() {
